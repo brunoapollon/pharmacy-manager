d7966312612a242677ba0929b1728e84ffb2e5ca
Bruno Lopes <eusoubruno7@gmail.com>
2022-01-27 14:11:07 -0300

    feat: adding create-funcionario migration

---
+++ src/database/migrations/1643302708638-create-funcionario.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class createFuncionario1643302708638 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`CREATE TABLE IF NOT EXISTS "funcionario"
+      ("cpf" varchar(14), "nome" varchar(50), "email" varchar(50),
+       "endereco" varchar(50), "telefone" varchar(20),
+       CONSTRAINT "func_pkey" PRIMARY KEY (cpf))`);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.query(`DROP TABLE IF EXISTS funcionario`);
+  }
+}
