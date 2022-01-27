155783dda28950e7473f67360fc095cd8a63e581
Bruno Lopes <eusoubruno7@gmail.com>
2022-01-27 11:56:03 -0300

    feat: adding eslint, prettier and editorconfig

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 
 app.use(express.json());
 
 app.listen(3333, () => {
-  console.log("server is running on port 3333 🛠️");
+  console.log('server is running on port 3333 🛠️');
 });
