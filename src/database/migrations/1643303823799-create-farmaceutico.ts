1c60085234aeea4153e8886635a31addbeaae77a
Bruno Lopes <eusoubruno7@gmail.com>
2022-01-27 14:19:52 -0300

    feat: adding create-farmaceutico migration

---
+++ src/database/migrations/1643303823799-create-farmaceutico.ts
@@ -0,0 +1,13 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class createFarmaceutico1643303823799 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.query(`CREATE TABLE IF NOT EXISTS farmaceutico
+      ("id_farmaceutico" integer, "cpf_funcionario" varchar(14),
+       CONSTRAINT "farmaceutico_pkey" PRIMARY KEY (id_farmaceutico))`);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.query(`DROP TABLE IF EXISTS farmaceutico`);
+  }
+}
